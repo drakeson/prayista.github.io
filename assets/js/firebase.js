036e7142ba223ba7f2e62352fd6acaadad3d3eb0
Kato Drake Smith <ryansmith7119@gmail.com>
2021-02-17 18:16:05 +0300

    nj

---
+++ assets/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBCyR3_eFghv2zD9ajyMAJWLIHscsGb3sc",
+    authDomain: "prayista-007.firebaseapp.com",
+    databaseURL: "https://prayista-007.firebaseio.com",
+    projectId: "prayista-007",
+    storageBucket: "prayista-007.appspot.com",
+    messagingSenderId: "455500668105",
+    appId: "1:455500668105:web:779efb4f73cefd72730118",
+    measurementId: "G-C93JQTXYCP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
